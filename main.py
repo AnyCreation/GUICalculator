258d6ac93c8c6b645fc3c0d1c1bc1e375596e329
AnyCreation <131777823+AnyCreation@users.noreply.github.com>
2024-05-23 00:06:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,12 +18,6 @@ def Num_Buttons(Start, End, W, H):
     button_width = W / 3 - 40
     button_height = H / 4 - 20
 
-    with dpg.theme(tag=4):
-        with dpg.theme_component(dpg.mvButton):
-            dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 8)
-            dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 10, 10)
-
-
     with dpg.group(horizontal=True, horizontal_spacing=5):
         S1 = dpg.add_button(label=f"{Start}", tag=f"{Start}", callback=Set_Back, width=button_width, height=button_height)
         dpg.bind_item_theme(S1, 4)
@@ -32,29 +26,33 @@ def Num_Buttons(Start, End, W, H):
         S3 = dpg.add_button(label=f"{End}", tag=f"{End}", callback=Set_Back, width=button_width, height=button_height)
         dpg.bind_item_theme(S3, 4)
 
-def Set_button(Start, End, W, H):
-    button_width = W / 3 - 40
-    button_height = H / 4 - 20
-
-    with dpg.theme(tag=4):
-        with dpg.theme_component(dpg.mvButton):
-            dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 8)
-            dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 10, 10)
-
+def Func_Buttons():
+    button_width = W / 5 - 40
+    button_height = 50
 
     with dpg.group(horizontal=True, horizontal_spacing=5):
-        S1 = dpg.add_button(label=f"{Start}", tag=f"{Start}", callback=Set_Back, width=button_width, height=button_height)
-        dpg.bind_item_theme(S1, 4)
-        S2 = dpg.add_button(label=f"{Start + 1}", tag=f"{Start + 1}", callback=Set_Back, width=button_width, height=button_height)
-        dpg.bind_item_theme(S2, 4)
-        S3 = dpg.add_button(label=f"{End}", tag=f"{End}", callback=Set_Back, width=button_width, height=button_height)
-        dpg.bind_item_theme(S3, 4)
+        F1 = dpg.add_button(label="+", tag="+", width=button_width, height=button_height)
+        dpg.bind_item_theme(F1, 4)
+        F2 = dpg.add_button(label="-", tag="-", width=button_width, height=button_height)
+        dpg.bind_item_theme(F2, 4)
+        F3 = dpg.add_button(label="x", tag="x", width=button_width, height=button_height)
+        dpg.bind_item_theme(F3, 4)
+        F4 = dpg.add_button(label="/", tag="/", width=button_width, height=button_height)
+        dpg.bind_item_theme(F4, 4)
 
 with dpg.window(label="Calculator", tag="Win"):
+     
+     with dpg.theme(tag=4):
+        with dpg.theme_component(dpg.mvButton):
+            dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 8)
+            dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 10, 10)
+
      Res = dpg.add_text("[...]")
-     Set_button(1, 3, W, H)
-     Set_button(4, 6, W, H)
-     Set_button(7, 9, W, H)
+     Num_Buttons(1, 3, W, H)
+     Num_Buttons(4, 6, W, H)
+     Num_Buttons(7, 9, W, H)
+
+     Func_Buttons()
 
 dpg.create_viewport(title='Calculator', width=W + 20, height=H)
 dpg.setup_dearpygui()
