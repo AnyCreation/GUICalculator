ab6c869e908c6ec6c32b4a1f956434e7b59a3954
AnyCreation <131777823+AnyCreation@users.noreply.github.com>
2024-05-22 16:36:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import dearpygui.dearpygui as dpg
 dpg.create_context()
 
 with dpg.window(label="Windows", width=800, height=800):
-    dpg.add_text("HELLO")
+    dpg.add_text("HELLO") 
     dpg.add_button(label="button")
     dpg.add_input_text(label="TEXT! HERE!")
     dpg.add_button(label="button")
