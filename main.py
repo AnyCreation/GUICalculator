a758f57c7812d9473a50ec65641b5d5358d8b15c
AnyCreation <131777823+AnyCreation@users.noreply.github.com>
2024-05-23 22:53:11 +0300

    Set_Rules. work

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ last_item = dpg.last_item()
 new_text = []
 new_func = ""
 
-def StrOrInt(Num, M):
+def StrOrInt(Num, M): # Namber --> int \ Rules --> str
     try:
         Num.append(int(M))
     except ValueError:
@@ -20,7 +20,7 @@ def StrOrInt(Num, M):
 
     return Num
 
-def Func_Push(Num, Func):
+def Func_Push(Num, Func): # Func-Rules
     M = 0
     if Func == "+":
         for i in Num:
@@ -43,7 +43,7 @@ def Func_Push(Num, Func):
             M = M ** Num[i]
 
     return M
-func = {
+func = { # Rules
     "[+]": lambda Num : Func_Push(Num, "+"),
     "[-]": lambda Num : Func_Push(Num, "-"),
     "[x]": lambda Num : Func_Push(Num, "x"),
@@ -51,28 +51,53 @@ func = {
     "[^]": lambda Num : Func_Push(Num, "^")
 }
 
-def Set_Rules(sender): # Replace Append "Rules" (Number (int) Or Function (str)) And "[...]"
-    global new_text, Fezmat
+def After_The_Decimal_Point(K1, K2):
+    return str(K1) + str(K2)
+
+def Set_Rules(sender): # Replace Append "Rules" (Number (int) Or Function (str)) And "[...]" /// Rule For Func Read In "F_count[rus/eng].txt"
+    global new_text
     if new_text == "[...]":
         new_text = []
-        Fezmat = StrOrInt(new_text, sender)
+        new_text = StrOrInt(new_text, sender)
     else:
-        Fezmat = StrOrInt(new_text, sender)
-    dpg.set_value(Use_Num, Fezmat)
+        new_text = StrOrInt(new_text, sender)
+
+    for Check in range(0, len(new_text) - 1): # Checks The Rules According To The "F_count[rus/eng].txt"
+        if type(new_text[Check]) in [int, float] and type(new_text[Check + 1]) in [int, float]: #Check Numbers Using Rules 1, 2, 3
+            K1, K2 = new_text[Check], new_text[Check + 1]
+            if K1 >= 1:
+                COS = K1 * 10 + K2
+                new_text.append(COS)
+            elif K1 > 0:
+                COS = float(After_The_Decimal_Point(K1, K2))
+                new_text.append(COS)   
+            else:
+                COS = K2 / 10
+                new_text.append(COS)
+                
+            if COS in new_text:
+                new_text.remove(K1)
+                new_text.remove(K2)
+
+        if type(new_text[Check]) == str and type(new_text[Check + 1]) == str: #Check Str Using Rules 5
+            K1 = new_text[Check]
+            new_text.remove(K1)
+            
+    dpg.set_value(Use_Num, new_text)
 
 def Delete_Number(sender): # For Button. Delete "Rules" And Set "[...]"
     global new_text, Use_Num
     new_text = "[...]"
     dpg.set_value(Use_Num, new_text)
 
-def Get_Result(sender): # Need To Checking The "Rules" (new_text) And Give Result /// Rule For Func Read In "F_count.TXT"
-    global Fezmat, new_func, Result
-    for Check in Fezmat: print(Check)
-
+def Get_Result(sender): # Need To Checking The "Rules" (new_text) And Give Result
+    global new_text, new_func, Result
+    pass
+        
 
 
 """ (| ------ ↓↓ Button Func (+, -, *, /, ^, DELETE and RESULT) And Number (1 - 9) ↓↓ --------------------------|) |X| """
-def Num_Buttons(Start, End, W, H): # Button (1 - 9)
+def Num_Buttons(Start, End, W, H): # Button (1 - 9 and 0)
     button_width = W / 3 - 40
     button_height = H / 4 - 20
 
@@ -83,6 +108,10 @@ def Num_Buttons(Start, End, W, H): # Button (1 - 9)
         dpg.bind_item_theme(S2, 4)
         S3 = dpg.add_button(label=f"{End}", tag=f"{End}", callback=Set_Rules, width=button_width, height=button_height)
         dpg.bind_item_theme(S3, 4)
+
+        if End == 9:
+            ZERO = dpg.add_button(label="0", tag=f"0", callback=Set_Rules, width=button_width, height=button_height)
+            dpg.bind_item_theme(ZERO, 4)
 
 def Func_Buttons(): # Function Button (+, -, *, /, ^, DELETE and RESULT)
     button_width = W / 5 - 40
@@ -123,14 +152,16 @@ with dpg.window(label="Calculator", tag="Win"): # Window
         with dpg.theme_component(dpg.mvButton):
             dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 8)
             dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 10, 10)
-
+    
      Result_Text()
-     T = "-" * (W // 5)
-     dpg.add_text(T)
+     
+     """T = "-" * (W // 5)
+     dpg.add_text(T)"""
+
      Num_Buttons(1, 3, W, H)
      Num_Buttons(4, 6, W, H)
      Num_Buttons(7, 9, W, H)
-
+     
      Func_Buttons()
 
 dpg.create_viewport(title='Calculator', width=W + 20, height=H)
